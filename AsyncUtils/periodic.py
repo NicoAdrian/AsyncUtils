0270cb612effa2a642df6f20d91707a5176bbfc7
NicoAdrian <nicolasadrian3@gmail.com>
2020-10-08 20:56:07 +0200

    use low level io loop call_at

--- AsyncUtils/periodic.py
+++ AsyncUtils/periodic.py
@@ -1,42 +1,53 @@
 import asyncio
-import time
+import logging
 
 
-class Periodic:
-    def __init__(self, cb, interval):
-        self.cb = cb
+class PeriodicCallback:
+    def __init__(self, callback, interval):
+        self.callback = callback
         self.interval = interval
-        self._fut = None
+        self._is_coro = asyncio.iscoroutinefunction(self.callback)
+        self._running = False
+        self._handle = None
 
     def start(self):
-        if not self.is_running():
-            self._fut = asyncio.ensure_future(self._run())
+        self._ioloop = asyncio.get_event_loop()
+        self._running = True
+        self._next_run = self._ioloop.time()
+        self._schedule_next()
 
     def stop(self):
-        if self.is_running():
-            asyncio.ensure_future(self._stop_fut())
+        self._running = False
+        if self._handle is not None:
+            self._handle.cancel()
+            self._handle = None
+
+    def _log_exc(self, ex):
+        logging.error("Exception in periodic callback %s: %s", self.callback, ex)
+
+    async def _run_coro(self):
+        try:
+            await self.callback()
+        except Exception as ex:
+            self._log_exc(ex)
 
     def is_running(self):
-        return self._fut is not None
+        return self._running
 
-    async def _stop_fut(self):
-        self._fut.cancel()
+    def _run(self):
+        if not self.is_running():
+            return
         try:
-            await self._fut
-        except asyncio.CancelledError:
-            pass
-        finally:
-            self._fut = None
-
-    async def _run(self):
-        to_wait = self.interval
-        is_coro = asyncio.iscoroutinefunction(self.cb)
-        while True:
-            if to_wait > 0:
-                await asyncio.sleep(to_wait)
-            t0 = time.time()
-            if is_coro:
-                await self.cb()
+            if self._is_coro:
+                asyncio.ensure_future(self._run_coro())
             else:
-                self.cb()
-            to_wait = self.interval - (time.time() - t0)
+                self.callback()
+        except Exception as ex:
+            self._log_exc(ex)
+        finally:
+            self._schedule_next()
+
+    def _schedule_next(self):
+        if self.is_running():
+            self._handle = self._ioloop.call_at(self._next_run, self._run)
+            self._next_run += self.interval
