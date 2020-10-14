d2033aa6f9c0182c8dd9e16835c9331f326c21f9
Nicolas Adrian <nicolasadrian3@gmail.com>
2020-10-14 21:14:21 +0200

    simplify periodic

--- AsyncUtils/periodic.py
+++ AsyncUtils/periodic.py
@@ -2,49 +2,32 @@ import asyncio
 import logging
 
 
-class PeriodicCallback:
+class Periodic:
     def __init__(self, callback, interval):
-        self.callback = callback
+        self.callback = asyncio.coroutine(callback)
         self.interval = interval
-        self._is_coro = asyncio.iscoroutinefunction(self.callback)
         self._running = False
-        self._handle = None
 
     def start(self):
         if not self.is_running():
             self._ioloop = asyncio.get_event_loop()
             self._running = True
-            self._next_run = self._ioloop.time()
-            self._schedule_next()
+            self._run()
 
     def stop(self):
         if self.is_running():
             self._handle.cancel()
             self._running = False
 
-    def _log_exc(self, ex):
-        logging.error("Exception in periodic callback %s: %s", self.callback, ex)
-
-    async def _run_coro(self):
+    async def _run_callback(self):
         try:
             await self.callback()
         except Exception as ex:
-            self._log_exc(ex)
+            logging.error("Exception in periodic callback '%s': %s", self.callback, ex)
 
     def is_running(self):
         return self._running
 
     def _run(self):
-        try:
-            if self._is_coro:
-                asyncio.ensure_future(self._run_coro())
-            else:
-                self.callback()
-        except Exception as ex:
-            self._log_exc(ex)
-        finally:
-            self._schedule_next()
-
-    def _schedule_next(self):
-        self._handle = self._ioloop.call_at(self._next_run, self._run)
-        self._next_run += self.interval
+        asyncio.ensure_future(self._run_callback())
+        self._handle = self._ioloop.call_later(self.interval, self._run)
